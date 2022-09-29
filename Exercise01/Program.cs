94f2b406713db0a9cf8dc38001c7e6d74d5a977d
Aivenest <aivenest@gmail.com>
2022-09-29 19:41:04 +0500

    ex01 compleated

---
+++ Exercise01/Program.cs
@@ -0,0 +1,28 @@
+﻿int ImputNumber()
+{
+    Console.WriteLine("Введите число 1:");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int ImputNumberTwo()
+{
+    Console.WriteLine("Введите число 2:");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double A = ImputNumber();
+
+double Multi(double N)
+{
+    double multi = 0;
+    for (double i = 1; i <= N; i++)
+    {
+        multi = Math.Pow(A,i);
+        Console.WriteLine(A + " в степени " + i + " равно: " + multi);
+    }
+    return multi;
+}
+
+
+double N =ImputNumberTwo();
+Multi(N);
